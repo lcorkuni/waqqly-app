edfe79fa02c33f44dddc277bd137f5c424bc36c0
Louis Cork <louis.cork@roke.co.uk>
2024-03-18 19:02:57 +0000

    cleanup

--- app.py
+++ app.py
@@ -1,54 +1,68 @@
 #!/usr/bin/env python3
-from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI, Request, Response, Cookie
+
+from typing import Optional
+from pathlib import Path
+from http import HTTPStatus
+from typing import Annotated
+from datetime import timedelta
+
+from fastapi import FastAPI, HTTPException, Request, Response, Cookie, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from pathlib import Path
-
 from starlette.datastructures import MutableHeaders
 
-from app_utils import *
+from app_utils import logging, UserType, User, get_password_hash, get_current_user, authenticate_user, \
+    create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES, fake_users_db
+
 
 app = FastAPI(title="Wagg.ly", description="A dog walkers app", version="1.0")
 templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory=Path(__file__).parent.absolute() / "static"), name="static")
+app.mount("/static", StaticFiles(directory=Path(__file__).parent.absolute() /
+          "static"), name="static")
 
 
 @app.middleware("http")
-async def add_auth_header_from_cookie(request: Request, call_next):
+async def authorization_middleware(request: Request, call_next):
+    """
+    Adds an access token to the "Authorization" header and required auth for docs
+    """
     access_token = request.cookies.get("access_token")
 
+    # Add Authorization to header from cookie
     if access_token and "Authorization" not in request.headers.keys():
         new_header = MutableHeaders(request._headers)
         new_header["Authorization"] = f"Bearer {access_token}"
         request._headers = new_header
         request.scope.update(headers=request.headers.raw)
 
-    response = await call_next(request)
-    return response
-
-
-@app.middleware("http")
-async def authenticate_docs_access(request: Request, call_next):
-    invalid_creds_response = JSONResponse(content={"details": "UNAUTHORIZED: Could not validate credentials"}, status_code=HTTPStatus.UNAUTHORIZED)
+    # Docs authorization
     try:
         if "/docs" in request.url.path:
-            token = request.cookies.get("access_token")
-            if token is None:
-                return invalid_creds_response
-            current_user = await get_current_user(token)
+            if access_token is None:
+                return JSONResponse(
+                    content={"details": "UNAUTHORIZED: No account is logged in"}, status_code=HTTPStatus.UNAUTHORIZED)
+            current_user = await get_current_user(access_token)
             if current_user.type != UserType.admin:
-                raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="User is not admin")
+                return JSONResponse(
+                    content={"details": "UNAUTHORIZED: User is not admin"}, status_code=HTTPStatus.UNAUTHORIZED)
     except HTTPException:
-        return invalid_creds_response
+        return JSONResponse(
+            content={"details": "UNAUTHORIZED: Could not validate credentials"}, status_code=HTTPStatus.UNAUTHORIZED)
 
     response = await call_next(request)
     return response
+
+
+# @app.middleware("http")
+# async def unauthorized_page(request: Request, call_next):
+#     response = await call_next(request)
+#     if response.status_code == HTTPStatus.UNAUTHORIZED:
+#         return templates.TemplateResponse(name="unauthorized.html", context={"details": response.details})
 
 
 @app.get("", include_in_schema=False, status_code=HTTPStatus.TEMPORARY_REDIRECT)
@@ -82,7 +96,8 @@ async def homepage(request: Request, current_user: Annotated[User, Depends(get_c
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ) -> JSONResponse:
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    user = authenticate_user(
+        fake_users_db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=HTTPStatus.UNAUTHORIZED,
@@ -95,25 +110,22 @@ async def login_for_access_token(
     )
     # Create a cookie for the token
     response = JSONResponse(content=access_token)
-    response.set_cookie(key="access_token", value=access_token)
-    response.set_cookie(key="username", value=user.username)
+    response.set_cookie(key="access_token", value=access_token, secure=True)
     return response
 
 
 @app.post("/logout")
 async def logout(response: Response):
     response.delete_cookie(key="access_token")
-    response.delete_cookie(key="username")
     return {"message": "Cookies cleared, successfully logged out."}
 
 
-@app.get("/get_cookies", response_class=JSONResponse)
-async def get_cookies(access_token: Optional[str] = Cookie(None), username: Optional[str] = Cookie(None)):
+@app.get("/get_cookie", response_class=JSONResponse)
+async def get_cookies(access_token: Optional[str] = Cookie(None)):
     try:
-        cookies = {"access_token": access_token, "username": username}
+        return {"access_token": access_token}
     except Exception:
-        cookies = "No cookies found"
-    return cookies
+        raise HTTPException("No cookie found", status_code=HTTPStatus.BAD_REQUEST)
 
 
 @app.get("/users/me/", response_model=User, tags=["Backend"])
